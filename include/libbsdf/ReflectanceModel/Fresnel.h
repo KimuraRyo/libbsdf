39eedd70bf3fb7906c0b075e2f4c23e89b001a72
KIMURA Ryo <ryki@mac.com>
2017-09-07 17:27:20 +0900

    Changed the names of reflectance function

--- include/libbsdf/ReflectanceModel/Fresnel.h
+++ include/libbsdf/ReflectanceModel/Fresnel.h
@@ -1,5 +1,5 @@
 // =================================================================== //
-// Copyright (C) 2015 Kimura Ryo                                       //
+// Copyright (C) 2015-2017 Kimura Ryo                                  //
 //                                                                     //
 // This Source Code Form is subject to the terms of the Mozilla Public //
 // License, v. 2.0. If a copy of the MPL was not distributed with this //
@@ -11,19 +11,27 @@
 
 #include <cmath>
 
+#include <libbsdf/Common/Vector.h>
+
 namespace lb {
 
 /*! Fresnel reflection. */
-float fresnelReflection(float inTheta, float n1, float n2);
+float fresnel(float inTheta, float n1, float n2);
 
 /*! Fresnel reflection. */
-float fresnelReflection(float inTheta, float n);
+float fresnel(float inTheta, float n);
+
+/*! Schlick's approximation. */
+float schlickFresnel(float inTheta, float n1, float n2 = 1.0f);
+
+/*! Schlick's approximation. */
+Vec3 schlickFresnel(float cosLN, const Vec3& R0);
 
 /*
  * Implementation
  */
 
-inline float fresnelReflection(float inTheta, float n1, float n2)
+inline float fresnel(float inTheta, float n1, float n2)
 {
     float cosi = std::cos(inTheta);
     float sint = n1 / n2 * std::sin(inTheta);
@@ -38,9 +46,22 @@ inline float fresnelReflection(float inTheta, float n1, float n2)
     return (Rs + Rp) / 2.0f;
 }
 
-inline float fresnelReflection(float inTheta, float n)
+inline float fresnel(float inTheta, float n)
 {
-    return fresnelReflection(inTheta, 1.0f, n);
+    return fresnel(inTheta, 1.0f, n);
+}
+
+inline float schlickFresnel(float inTheta, float n1, float n2)
+{
+    float r0 = (n1 - n2) / (n1 + n2);
+    float R0 = r0 * r0;
+
+    return R0 + (1.0f - R0) * std::pow(1.0f - std::cos(inTheta), 5.0f);
+}
+
+inline Vec3 schlickFresnel(float cosLN, const Vec3& R0)
+{
+    return R0 + (Vec3(1.0, 1.0, 1.0) - R0) * std::pow(1.0f - cosLN, 5.0f);
 }
 
 } // namespace lb
