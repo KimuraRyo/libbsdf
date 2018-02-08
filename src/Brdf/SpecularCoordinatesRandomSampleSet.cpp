3c9591dbba9f51b2aaf25dfbf2a0758679bd28de
KIMURA Ryo <ryki@mac.com>
2018-02-08 14:39:32 +0900

    Adjusted the parameter for estimation of sample points

--- src/Brdf/SpecularCoordinatesRandomSampleSet.cpp
+++ src/Brdf/SpecularCoordinatesRandomSampleSet.cpp
@@ -1,5 +1,5 @@
 // =================================================================== //
-// Copyright (C) 2016-2017 Kimura Ryo                                  //
+// Copyright (C) 2016-2018 Kimura Ryo                                  //
 //                                                                     //
 // This Source Code Form is subject to the terms of the Mozilla Public //
 // License, v. 2.0. If a copy of the MPL was not distributed with this //
@@ -19,6 +19,10 @@ void SpecularCoordinatesRandomSampleSet::setupBrdf(SpecularCoordinatesBrdf* brdf
     for (int inThIndex = 0; inThIndex < brdf->getNumInTheta();   ++inThIndex) {
     for (int inPhIndex = 0; inPhIndex < brdf->getNumInPhi();     ++inPhIndex) {
     for (int spThIndex = 0; spThIndex < brdf->getNumSpecTheta(); ++spThIndex) {
+        float angleInput3 = brdf->getInTheta(inThIndex) / SpecularCoordinateSystem::MAX_ANGLE0
+                          + brdf->getSpecTheta(spThIndex) / SpecularCoordinateSystem::MAX_ANGLE2;
+        angleInput3 /= 2.0;
+
         RandomSampleSet::AngleList angles;
         SampleMap::iterator it;
         float w3;
@@ -37,8 +41,9 @@ void SpecularCoordinatesRandomSampleSet::setupBrdf(SpecularCoordinatesBrdf* brdf
                                   it->second);
             }
             else {
-                // Modify a weight coefficient.
-                w3 = weight3 * hermiteInterpolation3(1.0f, 1.0f / weight3, brdf->getSpecTheta(spThIndex) / PI_2_F);
+                // Modify a weight coefficient for specular azimuthal angles.
+                w3 = hermiteInterpolation3(weight3, 10.0f * weight3, angleInput3);
+
                 sp = estimateSpectrum<SpecularCoordinateSystem>(angles, weight0, weight1, weight2, w3);
                 brdf->setSpectrum(inThIndex, inPhIndex, spThIndex, spPhIndex, sp);
             }
